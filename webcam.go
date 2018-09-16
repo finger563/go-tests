31450e625f2f4d306f3e8f9450ab31bd706d01ac
William Emfinger <waemfinger@gmail.com>
2018-09-16 18:12:57 -0500

    opencv webcam example.

---
+++ webcam.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	webcam, _ := gocv.OpenVideoCapture(0)
+	window := gocv.NewWindow("Hello")
+	img := gocv.NewMat()
+
+	for {
+		webcam.Read(&img)
+		window.IMShow(img)
+		if (window.WaitKey(1) == 27) {
+			break
+		}
+	}
+}

626038938924c3b7d4037fae9614586ac761121d
William Emfinger <waemfinger@gmail.com>
2018-09-17 09:34:30 -0500

    trying to enable dlib support in go.

--- webcam.go
+++ webcam.go
@@ -1,5 +1,10 @@
 package main
 
+// #cgo CPPFLAGS: -I../../../Downloads/dlib-19.15
+// #cgo LDFLAGS: -ldlib -lc++
+// #include "dlib/image_processing/frontal_face_detector.h"
+import "C"
+
 import (
 	"gocv.io/x/gocv"
 )
